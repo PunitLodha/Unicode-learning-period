0f7d1c2a687d990759259b7f8446289385ae56a6
PunitLodha <punitlodha@pm.me>
2019-12-01 12:08:01 +0530

    Cleaned Code

--- Week-3&4/reactcomponents/src/components/Switch.js
+++ Week-3&4/reactcomponents/src/components/Switch.js
@@ -5,8 +5,10 @@ const SwitchLabel = styled.label`
   position: relative;
   height: 40px;
   width: 70px;
+  cursor: pointer;
+  background-color: #fff;
+  border-radius: 30px;
   box-shadow: 0px 7px 64px 0px rgba(0, 0, 0, 0.07);
-  background-color: #f7f7f7;
 `;
 
 const SwitchInput = styled.input.attrs({ type: 'checkbox' })`
@@ -17,27 +19,16 @@ const SwitchInput = styled.input.attrs({ type: 'checkbox' })`
 
 const SwitchSpan = styled.span`
   position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #fff;
-  border-radius: 30px;
+  content: '';
+  height: 32px;
+  width: 32px;
+  left: 4px;
+  bottom: 4px;
+  background: #ecebed;
+  transition: 0.3s;
+  border-radius: 50%;
 
-  &::before {
-    position: absolute;
-    content: '';
-    height: 32px;
-    width: 32px;
-    left: 4px;
-    bottom: 4px;
-    background: #ecebed;
-    transition: 0.5s;
-    border-radius: 50%;
-  }
-
-  ${SwitchInput}:checked + &::before {
+  ${SwitchInput}:checked + & {
     transform: translateX(30px);
 
     background: linear-gradient(225deg, #bd7ae3 0%, #8461c9 100%);
