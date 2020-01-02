e8f194bc8575f125ed5b32752f80b05e6566781a
PunitLodha <punitlodha@pm.me>
2020-01-02 11:40:57 +0530

    added categories

--- Projects/quiz/src/api/triviaAPI.js
+++ Projects/quiz/src/api/triviaAPI.js
@@ -1,10 +1,27 @@
 import axios from 'axios';
 
-const getRandomQuestions = () => {
+const mapCatergoryToIDs = {
+  generalKnowledge: '9',
+  histroy: '23',
+  videoGames: '15',
+  sports: '21',
+  mythology: '20',
+  computer: '18',
+  maths: '19',
+  geography: '22',
+};
+
+let id;
+const getQuestions = category => {
+  if (category !== 'random') {
+    id = `&category=${mapCatergoryToIDs[category]}`;
+  } else {
+    id = '';
+  }
   const results = axios
-    .get('https://opentdb.com/api.php?amount=10&encode=url3986&type=multiple')
+    .get(`https://opentdb.com/api.php?amount=10&encode=url3986&type=multiple${id}`)
     .then(data => data);
   return results;
 };
 
-export default getRandomQuestions;
+export default getQuestions;
